9c390539bd8a3e4626d67526a92fab91a1d2a7ed
zhts148 <zhts148@163.com>
2017-08-28 15:15:56 +0800

    增加用户操作
    Signed-off-by: zhts148 <zhts148@163.com>

---
+++ TestCode2/Controllers/UserController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TestCode2.Model;
+
+namespace TestCode2.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/User")]
+    public class UserController : Controller
+    {
+        public string GetUserList()
+        {
+            string s = "";
+            List<UserModel> list = new List<UserModel>();
+            UserModel um = new UserModel()
+            {
+                ID = 1,
+                Name = "aaa"
+            };
+            UserModel um2 = new UserModel()
+            {
+                ID = 2,
+                Name = "bbb"
+            };
+            UserModel um3 = new UserModel()
+            {
+                ID = 3,
+                Name = "ccc"
+            };
+            UserModel um4 = new UserModel()
+            {
+                ID = 4,
+                Name = "ddd"
+            };
+            //UserModel um5 = new UserModel()
+            //{
+            //    ID = 5,
+            //    Name = "eee"
+            //};
+            list.Add(um);
+            list.Add(um2);
+            list.Add(um3);
+            list.Add(um4);
+            //list.Add(um5);
+            s = Newtonsoft.Json.JsonConvert.SerializeObject(list);
+            return s;
+        }
+    }
+}
---
+++ TestCode2/Model/UserModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestCode2.Model
+{
+    public class UserModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
