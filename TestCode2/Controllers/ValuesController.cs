28f754d2eafd8d6b089fbf9dfe8bd00bd9ecb339
zhts148 <zhts148@163.com>
2017-08-28 15:20:47 +0800

    反反复复

--- TestCode2/Controllers/ValuesController.cs
+++ TestCode2/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace TestCode2.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2", "value3" };
         }
 
         // GET api/values/5
